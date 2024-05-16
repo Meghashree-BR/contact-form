5e07754399b0dec93fe233b77fb7dffd789cf5e1
Meghashree-BR <meghashree8792@gamil.com>
2024-05-16 11:18:38 +0530

    hii

--- form.js
+++ form.js
@@ -4,8 +4,8 @@ document.getElementById('contact form')
     var name=document.getElementById('name').value;
     var email=document.getElementById('email').value;
     var message=document.getElementById('mesage).value;
-    var subject="new meassage from" + name;
+    var subject="new meassage from" + Meghashree;
     var body="name:" + "\nEmail:" +email + "\nMessage:" + message;
-    var mailto_link='mailto:its your mail id' + '?subject=' + subject + '&body=' + encodeURIComponent(body);
+    var mailto_link='mailto:meghashree8792@gmail.com' + '?subject=' + subject + '&body=' + encodeURIComponent(body);
     window.location.href=mailto_link;
 });
